baf5ff1d5df431f76ee72350c652dbe485c93e88
Alexander Rice <riceam@umsl.edu>
2019-04-26 12:50:24 -0500

    add basic HTML markup and webdev links

---
+++ homepage.js
@@ -0,0 +1,7 @@
+// I don't want my email visible in the index page source
+// Most bots don't have JavaScript enabled
+(()=>{
+const socialsLinks = document.querySelector('[data-hook="socials-links"]');
+const emailLink = '<li>Email: <a href="mailto:webdev@ricealexander.com">webdev@ricealexander.com</a></li>';
+socialsLinks.insertAdjacentHTML('beforeend', emailLink);
+})();
