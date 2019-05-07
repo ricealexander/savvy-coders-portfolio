888e3d461af153e02caaeaed53e9416eba293140
ricealexander <webdev@ricealexander.com>
2019-05-07 17:25:19 -0500

    refactor to handle navigation for all links

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,21 @@ export default function (state) {
   <nav class="nav container">
     <ul class="horizontal_list nav_list">
       <li class="nav_item">
-        <a class="nav_link" href="#">
+        <a class="nav_link" data-component="Home" href=".">
           <i class="material-icons nav_icon">home</i>
           Home
         </a>
       </li>
       <li class="nav_item">
-        <a class="nav_link" data-hook="projects-link" href="projects/index.html">
+        <a class="nav_link" data-component="Projects" href=".">
           <i class="material-icons nav_icon">dashboard</i>
           Projects
+        </a>
+      </li>
+      <li class="nav_item">
+        <a class="nav_link" data-component="Blog" href=".">
+          <i class="material-icons nav_icon">library_books</i>
+          Blog
         </a>
       </li>
     </ul>
--- index.js
+++ index.js
@@ -2,12 +2,20 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
-const home = {
-  title: 'Additional Links',
-};
-const projects = {
-  title: 'Additional Links (from projects page)',
-};
+const state = {
+  Blog: {
+    title: "Additional Links (from blog page)",
+  },
+  Home: {
+    title: "Additional Links",
+  },
+  Contact: {
+    title: "Additional Links (from contact page)",
+  },
+  Projects: {
+    title: "Additional Links (from projects page)",
+  }
+}
 
 function render(state) {
   const root = document.querySelector('[data-hook="root"]');
@@ -18,10 +26,19 @@ function render(state) {
   `;
 }
 
-render(home);
+render(state.Home);
 
-document.querySelector('[data-hook="projects-link"]')
-  .addEventListener("click", event => {
-    event.preventDefault(); // stops page reload
-    render(projects); // re-render on click
-});
+function handleNavigation(event) {
+  event.preventDefault();
+
+  const target = event.target.closest('.nav_link');
+  const component = target.getAttribute('data-component');
+
+  render(state[component]);
+}
+
+const navLinks = document.querySelectorAll(".top-navigation .nav_link");
+
+navLinks[0].addEventListener("click", handleNavigation);
+navLinks[1].addEventListener("click", handleNavigation);
+navLinks[2].addEventListener("click", handleNavigation);
