2e57a1c0fe1f6758bc1e95a64099aaac0ea3b576
ricealexander <webdev@ricealexander.com>
2019-05-08 18:44:20 -0500

    reinstall ESlint and apply linting to index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Import Components
-import Header from './components/Header';
+import Header  from './components/Header';
 import Content from './components/Content';
-import Footer from './components/Footer';
+import Footer  from './components/Footer';
 
 // Import Images
-import photoAvatar  from "./assets/avatar.png";
-import iconCodepen  from "./assets/svg/icon-codepen.svg";
-import iconCodewars from "./assets/svg/icon-codewars.svg";
-import iconGists    from "./assets/svg/icon-github-gists.svg";
-import iconGithub   from "./assets/svg/icon-github.svg";
-import iconLinkedin from "./assets/svg/icon-linkedin.svg";
+import photoAvatar  from './assets/avatar.png';
+import iconCodepen  from './assets/svg/icon-codepen.svg';
+import iconCodewars from './assets/svg/icon-codewars.svg';
+import iconGists    from './assets/svg/icon-github-gists.svg';
+import iconGithub   from './assets/svg/icon-github.svg';
+import iconLinkedin from './assets/svg/icon-linkedin.svg';
 
 const images = {
   photoAvatar,
@@ -18,49 +18,43 @@ const images = {
   iconGists,
   iconGithub,
   iconLinkedin,
-}
+};
 
+const root = document.querySelector('[data-hook="root"]');
 
-var root = document.querySelector('[data-hook="root"]');
-
-const state = {
-  Blog: {
-    title: "Additional Links (from blog page)",
-    images,
-  },
+const states = {
   Home: {
-    title: "Additional Links",
-    images,
-  },
-  Contact: {
-    title: "Additional Links (from contact page)",
+    title: 'Additional Links',
     images,
   },
   Projects: {
-    title: "Additional Links (from projects page)",
+    title: 'Additional Links (from projects page)',
     images,
-  }
-}
-
-function handleNavigation(event) {
-  event.preventDefault();
-
-  const target = event.target.closest('.nav_link');
-  const component = target.getAttribute('data-component');
-  render(state[component]);
-}
+  },
+  Blog: {
+    title: 'Additional Links (from blog page)',
+    images,
+  },
+};
 
 function render(state) {
+
+  function handleNavigation(event) {
+    event.preventDefault();
+
+    const target = event.target.closest('.nav_link');
+    const component = target.getAttribute('data-component');
+    render(states[component]);
+  }
+
   root.innerHTML = `
     ${Header(state)}
     ${Content(state)}
     ${Footer(state)}
   `;
 
-  const navLinks = document.querySelectorAll(".top-navigation .nav_link");
+  const navLinks = document.querySelectorAll('.top-navigation .nav_link');
   navLinks.forEach(navLink => navLink.addEventListener('click', handleNavigation));
 }
 
-render(state.Home);
-
-
+render(states.Home);
