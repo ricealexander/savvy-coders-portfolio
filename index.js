da6ee267101043c5b11ac0b4ff6c081efedd03d6
ricealexander <webdev@ricealexander.com>
2019-05-20 17:19:13 -0500

    refactor navigation handler to use navigo

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+// Import Dependencies
+import Navigo from 'navigo';
+
 // Import Components
-import Navigation  from './components/Navigation';
-import Content from './components/Content';
-import Footer  from './components/Footer';
+import Navigation from './components/Navigation';
+import Content    from './components/Content';
+import Footer     from './components/Footer';
 
 // Import States
 import * as states from './store';
 
 const root = document.querySelector('[data-hook="root"]');
+const router = new Navigo(window.location.origin);
 
-function render(state) {
-
-  function handleNavigation(event) {
-    event.preventDefault();
-    const target = event.target.closest('.nav_link');
-    const component = target.getAttribute('data-component');
-    render(states[component]);
-  }
-
+function startApp(state) {
   root.innerHTML = `
     ${Navigation(state)}
     ${Content(state)}
     ${Footer(state)}
   `;
-
-  const navLinks = document.querySelectorAll('.top-navigation .nav_link');
-  navLinks.forEach(navLink => navLink.addEventListener('click', handleNavigation));
+  router.updatePageLinks();
 }
 
-render(states.Home);
+function handleRoute(params) {
+  const page = params.path.charAt(0).toUpperCase() + params.path.slice(1);
+  startApp(states[page]);
+}
+
+router
+  .on('/:path', handleRoute)
+  .on('/', () => startApp(states.Home))
+  .resolve();
