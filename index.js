815c5aff6e0dc79a8de53ee04bfb83e49deac90a
ricealexander <webdev@ricealexander.com>
2019-10-21 20:35:03 -0500

    :rotating_light: Disable semicolons

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 // Import Dependencies
-import Navigo from 'navigo';
+import Navigo from 'navigo'
 // import { capitalize } from 'lodash';
 
 // Import Components
-import Navigation from './components/Navigation';
-import Content    from './components/Content';
-import Footer     from './components/Footer';
+import Navigation from './components/Navigation'
+import Content    from './components/Content'
+import Footer     from './components/Footer'
 
 // Import States
-import * as states from './store';
+import * as states from './store'
+import { db } from './firebase'
 
-const root = document.querySelector('[data-hook="root"]');
-const router = new Navigo(window.location.origin);
+const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
 
-function startApp(state) {
+const root = document.querySelector('[data-hook="root"]')
+const router = new Navigo(window.location.origin)
+
+function render(state) {
   root.innerHTML = `
     ${Navigation(state)}
     ${Content(state)}
     ${Footer(state)}
-  `;
-  router.updatePageLinks();
+  `
+  router.updatePageLinks()
 }
 
 function handleRoute(params) {
-  const page = params.path.charAt(0).toUpperCase() + params.path.slice(1);
-  startApp(states[page]);
+  const page = params.path
+  console.log(params)
+  render(states[capitalize(page)])
 }
 
 router
   .on('/:path', handleRoute)
-  .on('/', () => startApp(states.Home))
-  .resolve();
+  .on('/', () => render(states.Home))
+  .resolve()
+
+
+// axios
+//   .get('https://jsonplaceholder.typicode.com/posts')
+//   .then((response) => {
+//     // response.data contains an ARray of 100 post objects
+//     states.Blog.main = response.data.map(
+//       ({ title, body }) => `
+//       <article>
+//         <h2>${title}<h2>
+//         <p>${body}<p>
+//       </article>
+//     `,
+//     ).join('');
+
+//     if (capitalize(router.lastRouteResolved().params.page) === 'Blog') {
+//       render(states.Blog);
+//     }
+
+//   })
+//   .catch(err => console.log(err));
+
+
+// Gallery
+db.collection('images')
